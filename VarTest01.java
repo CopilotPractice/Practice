14c78d9af6f37e4e5b460b106eb76e45a63461e8
CopilotPractice <kch467@gmail.com>
2021-09-26 13:01:52 +0900

    education

---
+++ VarTest01.java
@@ -0,0 +1,19 @@
+
+public class VarTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num;
+		num = 4;
+		System.out.println(num);
+		
+		num = 13;
+		System.out.println(num);
+		
+		int num2 = 10;
+		System.out.println(num2);
+
+	}
+
+}
