9cd594de51f91ec1f7b19d8c94184c8d7c2ce195
CopilotPractice <kch467@gmail.com>
2021-09-26 13:05:48 +0900

    education

---
+++ CommentTest.java
@@ -0,0 +1,17 @@
+
+public class CommentTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("한번 출력됩니다!");
+		// System.out.println("한번 출력됩니다!");
+		
+		/*
+		System.out.println("한번 출력됩니다!");
+		System.out.println("한번 출력됩니다!");
+		System.out.println("한번 출력됩니다!");
+		*/
+		
+	}
+
+}
