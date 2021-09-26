8131fdbb35c67c251cf4551270709763ed6f208f
CopilotPractice <kch467@gmail.com>
2021-09-26 13:01:52 +0900

    education03

---
+++ GameCharTest.java
@@ -0,0 +1,22 @@
+
+public class GameCharTest {
+
+	public static void main(String[] args) {
+		
+		GameChar myMage;
+		myMage = new GameChar();
+		
+		myMage.lev = 1;
+		myMage.hp = 100;
+		myMage.name = "이쁜이마법사";
+		myMage.brood = "elf";
+		
+		GameChar myPaladin;
+		myPaladin = new GameChar();
+		
+		
+		
+		
+	}
+
+}
